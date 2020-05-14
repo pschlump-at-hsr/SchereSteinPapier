473c28910676205affd2fc365d74cd6779f3134e
Pascal Schlumpf <pascal.schlumpf@hispeed.ch>
2020-05-14 23:01:41 +0200

    design und startlogik implementiert

--- Umsetzung/js/main.js
+++ Umsetzung/js/main.js
@@ -1,35 +1,46 @@
-(
-  function(){
-  if (!('indexedDB' in window)) {
-    console.log('This browser doesn\'t support IndexedDB');
-    return;
-  }
+let player = new Map();
 
-  let openRequest = indexedDB.open("3SPB",1);
-
-openRequest.onupgradeneeded = function () {
-  let db = openRequest.result;
-  switch (db.version) {
-    case 0:
-      db.createObjectStore("player",{keyPath: 'name'});
-    case 1:
-      return;
+function init() {
+  if (localStorage.length > 0){
+    for(let i = 0;i < localStorage.length ; i++){
+      console.log("Spieler "+localStorage.key(i)+" mit "+localStorage.getItem(localStorage.key(i))+" Punkten hinzugefügt");
+      player.set(localStorage.key(i), localStorage.getItem(localStorage.key(i)));
+    }
   }
-  }
+  displayRanking();
+  document.getElementById('startGame').addEventListener('click', function () {
 
-openRequest.onerror = function () {
-  console.error("Error", openRequest.error);
+    let playerTextBox = document.getElementById('name');
+    let errmsg  = document.getElementById('errmsg');
+    if (playerTextBox.value < 3) {
+      errmsg.innerHTML="Mindestens 3 Zeichen eingeben!";
+    } else {
+      errmsg.innerHTML="";
+      if (typeof player.get(playerTextBox.value) == 'undefined') {
+        player.set(playerTextBox.value, 0);
+        localStorage.setItem(playerTextBox.value, '0');
+        console.log(playerTextBox.value + " wurde dem Storage hinzugefügt.");
+        displayRanking();
+      }
+      startGame(playerTextBox.value);
+    }
+  });
+}
+function startGame(activePlayer) {
+  document.getElementById("gamer").innerText = activePlayer;
 }
 
-openRequest.onsuccess = function () {
-  let db = openRequest.result;
-}
-}()
-
-)
-function start_game(player) {
-  if(player == 0){
-    window.alert("Bitte einen Namen eingeben!");
+function displayRanking() {
+  let ranking = "";
+  let rank = 1;
+  player[Symbol.iterator] = function* () {
+    yield* [...this.entries()].sort((a,b) => b[1]-a[1]);
   }
-
+  for (let [key, value] of player) {
+    console.log("Spieler "+key+" mit "+value+" Siegen");
+   ranking +="<li><h2>" + rank + ". Rang mit "+ value + " Punkten</h2><p>" + key + "</p></li>";
+    rank++;
+  }
+  document.getElementById('entries').innerHTML = ranking;
 }
+window.addEventListener('load', init);
